3acdf4ffd04912b1d53b30b6e9928b4c15c7dbe6
ferberga <55702143+ferberga@users.noreply.github.com>
2019-09-23 11:40:41 -0300

    Add files via upload

--- login.py
+++ login.py
@@ -1,8 +1,10 @@
 from tkinter import *
 tela = Tk()
+#criando a label login e definindo a linha e coluna q  vai ficar localizado
 lbllogin = Label(tela,text = 'login')
 lbllogin.grid(row=1,column=1)
 
+#Entrada de dados
 etrlogin = Entry(tela)
 etrlogin.grid(row=1, column=2)
 
@@ -12,7 +14,9 @@ lblsenha.grid(row=2,column=1)
 etrsenha = Entry(tela)
 etrsenha.grid(row=2, column=2)
 
+#criando botão e columnspan é a ocupação do botão (colunas)
 btnlogar = Button(tela,text = 'logar')
 btnlogar.grid(row=3,column=1,columnspan=2)
 
+#loop para que o programa só finalize a hora que fechar a TELA
 tela.mainloop()
